2382d631429e5e9a1b8e79644d0368101c37ff8a
manoj-hansda <me.manojhansda@gmail.com>
2016-08-09 00:41:59 +0530

    commit10

--- 7_Misc/Number_Manipulation/factorial.c
+++
@@ -1,5 +0,0 @@
-#inlcude<stdio.h>
- main()
- {
-
- }
--- 7_Misc/float_formats.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    float i=5;
-
-    printf("%f\n", i);
-    printf("%.f\n", i);
-    printf("%.2f\n", i);
-}
