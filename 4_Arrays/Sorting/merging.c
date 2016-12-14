abb807a613ab54990007e2bcefb5fdfd5a9077ea
Manoj <Manoj>
2016-12-14 23:38:24 +0530

    commit18

--- 4_Arrays/Sorting/merging.c
+++ 4_Arrays/Sorting/merging.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
-void sort(int *x);
-void merge(int *x, int *y);
-
 void main()
 {
-    int n1, n2, n3;
-    int i, j, k;
-
+    int n1, n2, i, j, k;
     printf("Enter length of array A: ");
     scanf("%d", &n1);
     int a[n1];
@@ -20,39 +15,40 @@ void main()
     scanf("%d", &n2);
     int b[n2];
 
-    printf("Enter %d elements:\n", n2);
+    printf("Enter %d elements:\n", n1);
     for(i=0; i<n2; i++)
         scanf("%d", &b[i]);
 
     int c[n1+n2];
 
-    int small = (n1<n2)? n1 : n2;
-    int diff = (n1<n2)? n2-n1 : n1-n2;
-
-    for(i=0, j=0, k=0; i<small; k++)
+    for(i=0, j=0, k=0 ; k<n1+n2 ; k++)
     {
-        if(a[i] <= b[j])
+        if(i < n1 && j < n2)
         {
-            c[k] = a[i];
-            i++;
+            if(a[i] <= b[j])
+            {
+                c[k] = a[i];
+                i++;
+            }
+            else
+            {
+                c[k] = b[j];
+                j++;
+            }
         }
-
-        else
+        else if(i == n1)
         {
             c[k] = b[j];
             j++;
         }
-
+        else if(j == n2)
+        {
+            c[k] = a[i];
+            i++;
+        }
     }
 
-    for(i=0; i<=diff; i++)
-    {
-        c[k] = b[j];
-        k++;
-        j++;
-    }
-
-    printf("Merged array A and B:\n");
+    printf("Merged array:\n");
     for(i=0; i<n1+n2; i++)
-        printf("%d ", c[i]);
+        printf("%d\n", c[i]);
 }
